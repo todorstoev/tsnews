d6e8eedb7051d155b65d352c2269ab29dec2cd0b
tstoev <todorsto@outlook.com>
2019-06-06 15:50:14 +0300

    change dark/light

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:tsnews/models/themes.dart' as Themes;
 
 void main() => runApp(MyApp());
+typedef void VoidCallback();
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   final title = "TSNEWS";
+  bool isLight = true;
+
+  void changeTheme() {
+    setState(() {
+      isLight = !this.isLight;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: title,
-        theme: Themes.MyThemes.lightTheme,
+        theme: isLight ? Themes.MyThemes.lightTheme : Themes.MyThemes.darkTheme,
         darkTheme: Themes.MyThemes.darkTheme,
         home: PageBuilder(
-          page: Home(),
+          page: Home(this.changeTheme),
         ));
   }
 }
 
 class Home extends StatelessWidget {
+  final VoidCallback changeTheme;
+
+  Home(this.changeTheme);
+
   @override
   Widget build(BuildContext context) {
-    return Text('Hello From Home');
+    return MaterialButton(
+      child: Text('Change Theme'),
+      onPressed: () {
+        this.changeTheme();
+      },
+    );
   }
 }
 
